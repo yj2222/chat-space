b034dd942375b779bf4e823a0fb01a1df624c0cf
yuji masujima <masjima2712@gmail.com>
2019-05-29 08:36:44 +0900

    Update group files

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
   def index
   end
-  
+
   def new
     @group = Group.new
     @group.users << current_user
@@ -16,8 +16,21 @@ class GroupsController < ApplicationController
     end
   end
 
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
+  end
+
   private
+
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
