9f1c65fc4e30092e0136ccd87dd1f2b8fbd3330c
yuji masujima <masjima2712@gmail.com>
2019-05-29 13:11:01 +0900

    Nest to model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :users, only: [:index, :edit, :update]
+  resource :user, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index]
+    resources :messages, only: [:index, :create]
   end
 end
