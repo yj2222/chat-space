66e1ba6c43e907a0405e8bd8b0c59c5183851645
yuji masujima <masjima2712@gmail.com>
2019-06-01 19:33:36 +0900

    Create message.js

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,58 @@
+$(function(){
+
+  function buildHTML(message){
+    image = ( message.image ) ? `<img class= "lower-message__image" src=${message.image} >` : "";
+      var html = 
+        `<div class="message">
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.date}
+            </div>
+          </div>
+          <div class="lower-meesage">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+            ${image}
+          </div>
+        </div>`
+      return html;
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      $('.form__message').val('');
+      $(".form__submit").prop('disabled', false);
+    })
+    .fail(function(){
+      alert('Please Type a Message!');
+    })
+  })
+});
+
+
+
+
+
+
+
+
+  
+  
