17e2d6f57985f69cbc452b32dc25235a956316bd
yuji masujima <masjima2712@gmail.com>
2019-06-06 17:01:37 +0900

    Update files
    インクルメンタルサーチの初回だけ動かない対応

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -34,7 +34,7 @@ $(function() {
   $(function(){
     $("#user-search-field").on("keyup", function() {
       var input = $("#user-search-field").val();
-
+      
       $.ajax({
         type: 'GET',
         url: '/users',
