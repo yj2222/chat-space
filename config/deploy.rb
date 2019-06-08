9b5a659898d98108a07421d20d9859f857ce50d6
yuji masujima <masjima2712@gmail.com>
2019-06-08 19:56:18 +0900

    Update

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,7 +47,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['<~/.ssh/key_Dair.pem']
+                  keys: ['~/.ssh/key_Dair.pem']
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
